5f5faaea8f8452b8273b1e3f9c18d85913df52cd
Quentin Feuvrais <feuvrais.quentin@gmail.com>
2022-12-16 11:13:26 +0100

    Skeleton done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import Home from "./pages/Home";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import SignIn from "../components/Login/SignIn";
+
+function Login() {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+}
+
+export default Login;
