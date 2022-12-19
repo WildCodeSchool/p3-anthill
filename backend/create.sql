1b2a8203dfedce3ef2656b0ab636b63bf36bb05b
Johanna <johanna.stoicanescu@gmail.com>
2022-12-19 15:27:55 +0100

    db

---
+++ backend/create.sql
@@ -0,0 +1,119 @@
+DROP DATABASE `p3-anthill-db`;
+
+CREATE DATABASE `p3-anthill-db`;
+
+USE `p3-anthill-db`;
+
+DROP TABLE IF EXISTS `badge`, `mood`, `user`, `topic`, `mindmap_mode`, `bubble`, `comment_mode`, `idea`, `comment`, `link`, `user_badge`, `user_topic`;
+
+CREATE TABLE `badge` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `picture` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`)
+); 
+
+CREATE TABLE `mood` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `emoji` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`)
+); 
+
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `picture` varchar(80) NOT NULL,
+  `email` varchar(70) NOT NULL,
+  `password` varchar(600) NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `mood_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+   CONSTRAINT `fk_user_mood` FOREIGN KEY (`mood_id`) REFERENCES `mood` (`id`)
+);
+
+CREATE TABLE `topic` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `deadline` datetime DEFAULT NULL,
+  `description` varchar(500) NOT NULL,
+  `is_private` tinyint(1) NOT NULL,
+  `creator_id` int NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `is_closed` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_topic_creator` FOREIGN KEY (`creator_id`) REFERENCES `user` (`id`)
+); 
+
+CREATE TABLE `mindmap_mode` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `topic_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_mindmap_mode_topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id`)
+); 
+
+CREATE TABLE `bubble` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `content` varchar(50) NOT NULL,
+  `mindmap_id` int NOT NULL,
+  `creator_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_bubble_mindmap` FOREIGN KEY (`mindmap_id`) REFERENCES `mindmap_mode` (`id`),
+  CONSTRAINT `fk_bubble_creator` FOREIGN KEY (`creator_id`) REFERENCES `user` (`id`)
+); 
+
+CREATE TABLE `comment_mode` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `topic_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_comment_mode_topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id`)
+); 
+
+CREATE TABLE `idea` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `like` int DEFAULT NULL,
+  `comment_mode_id` int DEFAULT NULL,
+  `creator_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+); 
+
+CREATE TABLE `comment` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `content` varchar(500) NOT NULL,
+  `like` int DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `idea_id` int NOT NULL,
+  `comment_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `idea` ADD CONSTRAINT `fk_idea_comment_mode` FOREIGN KEY (`comment_mode_id`) REFERENCES `comment_mode` (`id`);
+ALTER TABLE `idea` ADD CONSTRAINT `fk_idea_creator` FOREIGN KEY (`creator_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `comment` ADD CONSTRAINT `fk_comment_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+ALTER TABLE `comment` ADD CONSTRAINT `fk_comment_idea` FOREIGN KEY (`idea_id`) REFERENCES `idea` (`id`);
+ALTER TABLE `comment` ADD CONSTRAINT `fk_comment_comment` FOREIGN KEY (`comment_id`) REFERENCES `comment` (`id`);
+
+CREATE TABLE `link` (
+  `source_id` int NOT NULL,
+  `target_id` int NOT NULL,
+    CONSTRAINT `fk_link_source` FOREIGN KEY (`source_id`) REFERENCES `bubble` (`id`) ,
+  CONSTRAINT `fk_link_target` FOREIGN KEY (`target_id`) REFERENCES `bubble` (`id`)
+); 
+
+CREATE TABLE `user_badge` (
+  `user_id` int NOT NULL,
+  `badge_id` int NOT NULL,
+  CONSTRAINT `fk_user_badge_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `fk_user_badge_badge` FOREIGN KEY (`badge_id`) REFERENCES `badge` (`id`)
+);
+
+CREATE TABLE `user_topic` (
+  `user_id` int NOT NULL,
+  `topic_id` int NOT NULL,
+  CONSTRAINT `fk_user_topic_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `fk_user_topic_topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id`)
+);
+
+
+
