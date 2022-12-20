85618a998f4d528cc1c18450c63eff4bb3fbf20a
Herschel TUIL <herschel.tuil@gmail.com>
2022-12-20 18:24:11 +0100

    added fixtures for back-user-setup

--- backend/create.sql
+++ backend/create.sql
@@ -13,12 +13,16 @@ CREATE TABLE `badge` (
   PRIMARY KEY (`id`)
 ); 
 
+INSERT INTO badge (name, picture) VALUES ("badge_name_1", "badge_picture_1");
+
 CREATE TABLE `mood` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `emoji` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ); 
+
+INSERT INTO mood (name, emoji) VALUES ("mood_name_1", "mood_emoji_1");
 
 CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -32,6 +36,8 @@ CREATE TABLE `user` (
    CONSTRAINT `fk_user_mood` FOREIGN KEY (`mood_id`) REFERENCES `mood` (`id`)
 );
 
+INSERT INTO user (picture, email, password, fullname, googleUserId, mood_id) VALUES ("picture_user_1", "email_1@gmail.com", "user_password_1", "user_fullname_1", "user_google_id_1", 1);
+
 CREATE TABLE `topic` (
   `id` int NOT NULL AUTO_INCREMENT,
   `deadline` datetime DEFAULT NULL,
@@ -44,12 +50,16 @@ CREATE TABLE `topic` (
   CONSTRAINT `fk_topic_creator` FOREIGN KEY (`creator_id`) REFERENCES `user` (`id`)
 ); 
 
+INSERT INTO topic (deadline, description, is_private, creator_id, title, is_closed) VALUES (curdate(), "topic_description_1", 0, 1, "topic_title_1", 0);
+
 CREATE TABLE `mindmap_mode` (
   `id` int NOT NULL AUTO_INCREMENT,
   `topic_id` int NOT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_mindmap_mode_topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id`)
 ); 
+
+INSERT INTO mindmap_mode (topic_id) VALUES (1);
 
 CREATE TABLE `bubble` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -61,12 +71,16 @@ CREATE TABLE `bubble` (
   CONSTRAINT `fk_bubble_creator` FOREIGN KEY (`creator_id`) REFERENCES `user` (`id`)
 ); 
 
+INSERT INTO bubble (content, mindmap_id, creator_id) VALUES ("bubble_content_1", 1, 1);
+
 CREATE TABLE `comment_mode` (
   `id` int NOT NULL AUTO_INCREMENT,
   `topic_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_comment_mode_topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id`)
 ); 
+
+INSERT INTO comment_mode (topic_id) VALUES (1);
 
 CREATE TABLE `idea` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -78,15 +92,19 @@ CREATE TABLE `idea` (
   PRIMARY KEY (`id`)
 ); 
 
+INSERT INTO idea (title, description, upVote, comment_mode_id, creator_id) VALUES ("idea_title_1", "idea_description_1", 1, 1, 1);
+
 CREATE TABLE `comment` (
   `id` int NOT NULL AUTO_INCREMENT,
   `content` varchar(500) NOT NULL,
-  `like` int DEFAULT NULL,
+  `likes` int DEFAULT NULL,
   `user_id` int NOT NULL,
   `idea_id` int NOT NULL,
   `comment_id` int DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
+
+INSERT INTO comment (content, likes, user_id, idea_id, comment_id) VALUES ("comment_content_1", 1, 1, 1, 1);
 
 ALTER TABLE `idea` ADD CONSTRAINT `fk_idea_comment_mode` FOREIGN KEY (`comment_mode_id`) REFERENCES `comment_mode` (`id`);
 ALTER TABLE `idea` ADD CONSTRAINT `fk_idea_creator` FOREIGN KEY (`creator_id`) REFERENCES `user` (`id`);
@@ -103,6 +121,8 @@ CREATE TABLE `link` (
   CONSTRAINT `fk_link_target` FOREIGN KEY (`target_id`) REFERENCES `bubble` (`id`)
 ); 
 
+INSERT INTO link (source_id, target_id) VALUES (1, 1);
+
 CREATE TABLE `user_badge` (
   `user_id` int NOT NULL,
   `badge_id` int NOT NULL,
@@ -111,6 +131,8 @@ CREATE TABLE `user_badge` (
   CONSTRAINT `fk_user_badge_badge` FOREIGN KEY (`badge_id`) REFERENCES `badge` (`id`)
 );
 
+INSERT INTO user_badge (user_id, badge_id) VALUES (1, 1);
+
 CREATE TABLE `user_topic` (
   `user_id` int NOT NULL,
   `topic_id` int NOT NULL,
@@ -118,6 +140,10 @@ CREATE TABLE `user_topic` (
   CONSTRAINT `fk_user_topic_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `fk_user_topic_topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id`)
 );
+
+INSERT INTO user_topic (user_id, topic_id) VALUES (1, 1);
+
+
 
 
 
