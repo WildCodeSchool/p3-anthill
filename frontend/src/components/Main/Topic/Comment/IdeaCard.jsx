679728b829d8fbc77efd3c58301dc5a6ca69a76f
audja <audjacontact@gmail.com>
2023-01-03 21:02:26 +0100

    add icons ideacards

--- frontend/src/components/Main/Topic/Comment/IdeaCard.jsx
+++ frontend/src/components/Main/Topic/Comment/IdeaCard.jsx
@@ -1,3 +1,6 @@
+import { BiUpvote } from "react-icons/bi";
+import { FaCommentAlt } from "react-icons/fa";
+
 function IdeaCard({ title, creatorName, description, nbUpVote, nbComment }) {
   return (
     <div className="ideaCard">
@@ -7,8 +10,12 @@ function IdeaCard({ title, creatorName, description, nbUpVote, nbComment }) {
       </div>
       <p className="ideaCard__description">{description}</p>
       <div className="ideaCard__interactions">
-        <div className="ideaCard__nbUpVote">{nbUpVote}</div>
-        <div className="ideaCard__nbComment">{nbComment}</div>
+        <div className="ideaCard__nbUpVote">
+          {nbUpVote} <BiUpvote />
+        </div>
+        <div className="ideaCard__nbComment">
+          {nbComment} <FaCommentAlt />
+        </div>
       </div>
     </div>
   );
