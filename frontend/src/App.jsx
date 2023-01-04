859eff29b6a2ebf981672c21b5f745710ebb391f
Cylecq <leclercq.cyril06@gmail.com>
2023-01-04 13:00:25 +0100

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,9 @@ import "./App.css";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import TopicCommentDetails from "./pages/Dashboard/Pages/TopicCommentDetails";
 import Main from "./pages/Dashboard/Layout/Main";
 import TopicsPage from "./pages/Dashboard/Pages/TopicList";
+import TopicCommentDetails from "./pages/Dashboard/Pages/TopicCommentDetails";
 
 function App() {
   return (
