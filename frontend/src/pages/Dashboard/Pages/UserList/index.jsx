91b48cb765db4b74ebec3639137033f418dda2bb
Herschel TUIL <herschel.tuil@gmail.com>
2023-01-05 15:05:29 +0100

    added new styles grid/flex in userList

--- frontend/src/pages/Dashboard/Pages/UserList/index.jsx
+++ frontend/src/pages/Dashboard/Pages/UserList/index.jsx
@@ -1,17 +1,25 @@
+import { useContext } from "react";
 import useFetch from "../../../../services/useFetch";
 import UserCard from "../../Components/UserCard";
 import ToggleMode from "../../Components/ToggleMode";
+import ToggleModeContext from "../../../../contexts/ToggleModeContext";
+import "./UserList.css";
 
 export default function UserList() {
   const { data: users, loading } = useFetch("/users");
+  const { toggleMode } = useContext(ToggleModeContext);
   return (
     <div>
       <ToggleMode />
       {loading && <div>LOADING...</div>}
-      {users.length &&
-        users.map((el) => {
-          return <UserCard key={el.id} user={el} />;
-        })}
+      <div
+        className={!toggleMode ? "userList__list__main" : "userList_grid__main"}
+      >
+        {users.length &&
+          users.map((el) => {
+            return <UserCard key={el.id} user={el} />;
+          })}
+      </div>
     </div>
   );
 }
