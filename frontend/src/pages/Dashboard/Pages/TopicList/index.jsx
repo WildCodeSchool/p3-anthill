cf20670cdf5a8acf8e5ffbd51c822430175c797e
Huy Trinh <laser2u02@gmail.com>
2023-01-06 17:20:39 +0100

    fix favicon

--- frontend/src/pages/Dashboard/Pages/TopicList/index.jsx
+++ frontend/src/pages/Dashboard/Pages/TopicList/index.jsx
@@ -10,21 +10,21 @@ function TopicsList() {
   return (
     <div>
       <ToggleModeButtons />
-    <div className="topicsList">
-      {loading && <div>LOADING...</div>}
-      {topics &&
-        topics.map((topic) => (
-          <TopicCard
-            key={topic.id}
-            id={topic.id}
-            title={topic.title}
-            creatorName={topic.creator_name}
-            description={topic.description}
-            deadline={topic.deadline}
-            nbIdea={topic.nb_idea}
-          />
-        ))}
-    </div>
+      <div className="topicsList">
+        {loading && <div>LOADING...</div>}
+        {topics &&
+          topics.map((topic) => (
+            <TopicCard
+              key={topic.id}
+              id={topic.id}
+              title={topic.title}
+              creatorName={topic.creator_name}
+              description={topic.description}
+              deadline={topic.deadline}
+              nbIdea={topic.nb_idea}
+            />
+          ))}
+      </div>
     </div>
   );
 }
