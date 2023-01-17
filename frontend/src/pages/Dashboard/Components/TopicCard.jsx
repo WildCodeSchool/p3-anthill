9ebdd7485b9ae6cddf27e2bd0aeb9a750986efe3
Cylecq <leclercq.cyril06@gmail.com>
2023-01-17 14:56:26 +0100

    Some css, j'ai abandonné parce que johanna veut le faire et ça me fait chier

--- frontend/src/pages/Dashboard/Components/TopicCard.jsx
+++ frontend/src/pages/Dashboard/Components/TopicCard.jsx
@@ -1,12 +1,22 @@
-import { FaCommentAlt } from "react-icons/fa";
+import { BiMessageDetail } from "react-icons/bi";
 import { RxLapTimer } from "react-icons/rx";
+import { TbMessageCircle2 } from "react-icons/tb";
 import { Link } from "react-router-dom";
 import formatDeadline from "../../../services/formatDeadline";
 
 import "./TopicCard.css";
 
 function TopicCard(props) {
-  const { id, title, creatorName, description, deadline, nbIdea } = props;
+  const {
+    id,
+    title,
+    creatorName,
+    description,
+    deadline,
+    nbIdea,
+    nbBubble,
+    isCommentMode,
+  } = props;
 
   const formatedDeadLine = formatDeadline(deadline);
   const [year, month, day, hour, minutes] = [...formatedDeadLine];
@@ -24,9 +34,17 @@ function TopicCard(props) {
           <RxLapTimer />
           <p>{`${day}/${month}/${year} at ${hour}h${minutes}`}</p>
         </div>
-        <div className="topicCard__nbIdea">
-          {nbIdea} <FaCommentAlt />
-        </div>
+        {isCommentMode ? (
+          <div className="topicCard__nbIdea notification">
+            <div className="notification-number">{nbIdea}</div>
+            <BiMessageDetail />
+          </div>
+        ) : (
+          <div className="topicCard__nbIdea notification">
+            <div className="notification-number">{nbBubble}</div>
+            <TbMessageCircle2 />
+          </div>
+        )}
       </div>
     </article>
   );
