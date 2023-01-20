3f33b9354857da1b2ba1939310b7ac6c5e23026f
Huy Trinh <laser2u02@gmail.com>
2023-01-20 02:37:53 +0100

    section nav and structure landing

--- frontend/src/pages/Landing/Components/NavLanding.jsx
+++ frontend/src/pages/Landing/Components/NavLanding.jsx
@@ -1,38 +1,59 @@
+import { useEffect, useRef } from "react";
+
 import BackgrdParticles from "../../../assets/BackgrdParticles/BackgrdParticles";
 import Logo from "../../../assets/Logo/Logo";
 import "./NavLanding.css";
 
+const {
+  gsap: { set, to },
+} = window;
+
 function NavLanding() {
+  const textRef = useRef(null);
+  const stageRef = useRef(null);
+
+  useEffect(() => {
+    set(stageRef.current, { autoAlpha: 1 });
+
+    set(textRef.current, {
+      transformOrigin: "center bottom",
+      y: -500,
+    });
+
+    to(textRef.current, {
+      y: 80,
+      fontWeight: 600,
+      fontStretch: 40,
+      scaleY: 1,
+      ease: "elastic(0.3, 0.1)",
+      duration: 1.5,
+      delay: 0.5,
+      stagger: {
+        each: 0.05,
+        from: "random",
+      },
+    });
+  }, []);
+
   return (
     <div className="nav-landing">
       <BackgrdParticles />
-      <h1>Ant Hill</h1>
-      <h3 className="span loader">
-        <span className="m">B</span>
-        <span className="m">E</span>
-        <span className="m">N</span>
-        <span className="m">E</span>
-        <span className="m">F</span>
-        <span className="m">I</span>
-        <span className="m">T</span>
-        <span className="m">S</span>
-        <span className="m">&nbsp;</span>
-        <span className="m">o</span>
-        <span className="m">f</span>
-        <span className="m">&nbsp;</span>
-        <span className="m">T</span>
-        <span className="m">E</span>
-        <span className="m">C</span>
-        <span className="m">H</span>
-        <span className="m">N</span>
-        <span className="m">O</span>
-        <span className="m">L</span>
-        <span className="m">O</span>
-        <span className="m">G</span>
-        <span className="m">I</span>
-        <span className="m">E</span>
-        <span className="m">S</span>
-      </h3>
+      <div className="stage">
+        <div className="content" ref={stageRef}>
+          <h1 className="txt" ref={textRef}>
+            AntHill
+          </h1>
+        </div>
+        <div>
+          <h3 className="span loader">
+            <span className="m">Make</span>
+            <span className="m">Ideas</span>
+            <span className="m">Brilli</span>
+            <span className="m">-</span>
+            <span className="m">Ant</span>
+          </h3>
+        </div>
+      </div>
       <div className="nav-container">
         <Logo />
         <a className="nav-tab" href="#tab-intro">
