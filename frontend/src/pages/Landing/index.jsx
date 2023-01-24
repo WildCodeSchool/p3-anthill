11b2ea0b06210e44884e5ab1781ffc677fd7b24a
Huy Trinh <laser2u02@gmail.com>
2023-01-24 11:31:34 +0100

    scroll and stick nav bar

--- frontend/src/pages/Landing/index.jsx
+++ frontend/src/pages/Landing/index.jsx
@@ -1,31 +1,34 @@
 import { Link } from "react-router-dom";
+import scrollController from "./Components/scrollController";
+
 import NavLanding from "./Components/NavLanding";
 import Footer from "./Components/footer/Footer";
 
 import "./index.css";
 
 function Landing() {
+  const { isSticky, element } = scrollController();
   return (
     <div className="landing">
-      <NavLanding />
-      <div className="main">
-        <section className="slider" id="tab-intro">
+      <NavLanding sticky={isSticky} />
+      <div ref={element} className="main">
+        <section className="slider" id="section1">
           <h1>INTRO</h1>
           <h2>About our website</h2>
         </section>
-        <section className="slider" id="tab-dash">
+        <section className="slider" id="section2">
           <h1>DASHBOARD</h1>
           <h2>introduce the operations</h2>
         </section>
-        <section className="slider" id="tab-next">
+        <section className="slider" id="section3">
           <h1>FEATURE</h1>
           <h2>framework for Production</h2>
         </section>
-        <section className="slider" id="tab-typescript">
+        <section className="slider" id="section4">
           <h1>CUSTOMER</h1>
           <h2>giving you better viewer for the future</h2>
         </section>
-        <section className="slider-us" id="tab-aboutUs">
+        <section className="slider-us" id="section5">
           <Footer />
         </section>
       </div>
