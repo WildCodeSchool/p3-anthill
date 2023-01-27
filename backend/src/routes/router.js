d10d22a597af5cba5c837818b3e9a48bfd7b31bc
Herschel TUIL <herschel.tuil@gmail.com>
2023-01-27 12:32:55 +0100

    jpp

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -8,7 +8,6 @@ const { moodRouter } = require("./mood.route");
 const { voteRouter } = require("./vote.route");
 const { bubbleRouter } = require("./bubble.route");
 const { slackRouter } = require("./slack.route");
-const { bubbleRouter } = require("./bubble.route");
 
 const router = express.Router();
 
