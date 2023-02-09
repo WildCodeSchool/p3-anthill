d7deea2f5c9cb51f2224aa317c50ac18f3484c5a
Johanna <johanna.stoicanescu@gmail.com>
2023-02-09 16:50:11 +0100

    search bar css

--- frontend/src/pages/Dashboard/Components/SearchBar/SearchBar.jsx
+++ frontend/src/pages/Dashboard/Components/SearchBar/SearchBar.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { BiSearchAlt } from "react-icons/bi";
+import "./SearchBar.css";
 
 function SearchBar() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -12,14 +14,21 @@ function SearchBar() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={handleChange}
-        placeholder="Rechercher..."
-      />
-      <button type="submit">Rechercher</button>
+    <form onSubmit={handleSubmit} className="searchBar__form">
+      <div className="searchBar__container">
+        <BiSearchAlt className="searchBar__icon" />
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleChange}
+          placeholder="Recherche"
+          className="searchBar__input"
+        />
+      </div>
+
+      <button type="submit" className="searchBar__button">
+        Search
+      </button>
     </form>
   );
 }
